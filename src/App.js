1a1f77550eb18a87e4dbc2397a5a1c234127d0cc
SumeyraAltinisik <sfa2023@gmail.com>
2023-03-21 20:06:14 +0300

    Cards projesi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import 'bulma/css/bulma.css';
 import './App.css';
+import Course  from './Course';
+import Angular from './images/angular.jpg';
+import Bootstrap from './images/bootstrap5.png';
+import Csharp from './images/ccsharp.png';
+import KompleWeb from './images/kompleweb.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Kurslarım</p>
+        </div>
+      </section>
+      <div className='container'>
+        <section className='section'>
+        <div className='columns'>
+          <div className='column'>
+          <Course 
+            image={Angular}
+            title="Angular"
+            description="Lorem"
+          />
+          </div>
+          <div className='column'>
+          <Course 
+            image={Bootstrap}
+            title="Bootstrap5" 
+            description="Lorem"
+          />
+          </div>
+          <div className='column'>
+          <Course 
+            image={KompleWeb}
+            title="Komple Web" 
+            description="Lorem"
+          />
+          </div>
+          <div className='column'>
+          <Course 
+            image={Csharp}
+            title="Frontend" 
+            description="Lorem"
+          />
+          </div>
+        </div>
+        </section>
+      </div>     
     </div>
   );
 }
